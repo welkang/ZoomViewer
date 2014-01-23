bd9a830643cdb9b823787ce3dc4a8940f436c95a
welkang <welkang@qq.com>
2014-01-23 11:56:18 +0800

    jquey plugin mode

--- zoom-viewer.js
+++ zoom-viewer.js
@@ -19,94 +19,120 @@
     ImageOffsetY
 */
 
-$(function(){
-    $container = $('.zoombox');
-    $image = $('.zoombox img');
-    var ConatainerW = $container.width();
-    var ConatainerH = $container.height();
+(function($){
 
-    var ImageW = $image.width();
-    var ImageH = $image.height();
+    $.fn.zoomViewer = function(options){
 
-    var ImageOffsetX = 0;
-    var ImageOffsetY = 0;
+        var settings = $.extend({}, options);
 
-    var zoomPercent = 0;
+        return this.each(function(){
 
-    if(ImageW >= ImageH){ // landscape
-        $image.width(ConatainerW <= ImageW ? ConatainerW : ImageW);
-        ImageOffsetY = (ConatainerH - $image.height())/2;
-        $image.css('top', ImageOffsetY);
-    }else{ // portrait
-        $image.height(ConatainerH <= ImageH ? ConatainerH : ImageH);
-        ImageOffsetX = (ConatainerW - $image.width())/2;
-        $image.css('left', ImageOffsetX);
-    }
+            $zoombox = $(this);
+            $image = $(this).find('img');
+            var ConatainerW = $zoombox.width();
+            var ConatainerH = $zoombox.height();
 
-    zoomPercent = $image.width()/ImageW;
-    // console.log(zoomPercent);
+            var ImageW = $image.width();
+            var ImageH = $image.height();
 
+            var ImageOffsetX = 0;
+            var ImageOffsetY = 0;
 
-    // zoomImg($image, zoomPercent+0.1);
+            var zoomPercent = 0;
+            var angle = 0;
 
-    function zoomImg (img, percent) {
-        // body...
-        ImageOffsetX -= (ImageW*percent - img.width())/2;
-        ImageOffsetY -= (ImageH*percent - img.height())/2;
+            if(ImageW >= ImageH){ // landscape
+                $image.width(ConatainerW <= ImageW ? ConatainerW : ImageW);
+                ImageOffsetY = (ConatainerH - $image.height())/2;
+                $image.css('top', ImageOffsetY);
+            }else{ // portrait
+                $image.height(ConatainerH <= ImageH ? ConatainerH : ImageH);
+                ImageOffsetX = (ConatainerW - $image.width())/2;
+                $image.css('left', ImageOffsetX);
+            }
 
-        img.width(ImageW*percent);
-        img.height(ImageH*percent);
+            zoomPercent = $image.width()/ImageW;
 
+            function zoomImg (img, percent) {
+                // body...
+                ImageOffsetX -= (ImageW*percent - img.width())/2;
+                ImageOffsetY -= (ImageH*percent - img.height())/2;
 
-        img.css('left', ImageOffsetX);
-        img.css('top', ImageOffsetY);
-    }
+                img.width(ImageW*percent);
+                img.height(ImageH*percent);
 
-
-    // Mousewheel Event
-    $('.zoombox').bind('mousewheel', function(event){
-
-        if(event.wheelDelta > 0){
-            zoomPercent += 0.1;
-            zoomImg($image, zoomPercent);
-        }else{
-            if(zoomPercent <= 0.1){
-                return;
+                img.css('left', ImageOffsetX);
+                img.css('top', ImageOffsetY);
             }
 
 
-            // Todo: 缩小时往中心靠拢
-            if($image.width() - $container.width() <= 100){
-                ImageOffsetX = (ConatainerW-$image.width())/2;
-                ImageOffsetY = (ConatainerH-$image.height())/2;
+            // Mousewheel Event
+            $zoombox.bind('mousewheel', function(event){
+
+                if(event.wheelDelta > 0){
+                    zoomPercent += 0.1;
+                    zoomImg($image, zoomPercent);
+                }else{
+                    if(zoomPercent <= 0.1){
+                        return;
+                    }
+
+
+                    // Todo: 缩小时往中心靠拢
+                    if($image.width() - $zoombox.width() <= 100){
+                        ImageOffsetX = (ConatainerW-$image.width())/2;
+                        ImageOffsetY = (ConatainerH-$image.height())/2;
+                    }
+
+                    zoomPercent -= 0.1;
+                    zoomImg($image, zoomPercent);
+                }
+            });
+
+
+            // Drag Move Event
+            $zoombox.get(0).onmousedown = function(e){
+                var last = e;
+                e.preventDefault();
+
+                function drag(e) {
+                    e.preventDefault();
+                    ImageOffsetX += (e.pageX - last.pageX);
+                    ImageOffsetY += (e.pageY - last.pageY);
+                    last = e;
+
+                    $image.css({'left':ImageOffsetX, 'top':ImageOffsetY});
+                }
+
+                $(document)
+                .bind('mousemove', drag)
+                .one('mouseup', function () {
+                    $(document).unbind('mousemove', drag);
+                });
+            };
+
+
+            // Rotate Event
+            function rotateLeft(argument){
+                // body...
+                angle -= 90;
+                $image.css('-webkit-transform', 'rotate('+angle+'deg)');
+            }
+            function rotateRight(argument){
+                // body...
+                angle += 90;
+                $image.css('-webkit-transform', 'rotate('+angle+'deg)');
             }
 
-            zoomPercent -= 0.1;
-            zoomImg($image, zoomPercent);
-        }
-    });
+            // $('.js_rotateLeft').click(function(){
+            //     rotateLeft(); 
+            // })
+            // $('.js_rotateRight').click(function(){
+            //     rotateRight(); 
+            // })            
+
+        })
+    }
 
 
-    // Drag Move Event
-    $('.zoombox').get(0).onmousedown = function(e){
-        var last = e;
-
-        e.preventDefault();
-
-        function drag(e) {
-            e.preventDefault();
-            ImageOffsetX += (e.pageX - last.pageX);
-            ImageOffsetY += (e.pageY - last.pageY);
-            last = e;
-
-            $image.css({'left':ImageOffsetX, 'top':ImageOffsetY});
-        }
-
-        $(document)
-        .bind('mousemove', drag)
-        .one('mouseup', function () {
-            $(document).unbind('mousemove', drag);
-        });
-    };
-
-});
+})(jQuery);
